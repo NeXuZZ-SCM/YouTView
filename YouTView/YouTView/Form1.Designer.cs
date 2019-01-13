d201ae636e7384b2e9eaa10077b1cbf8be958d2b
nexuzz <nexuzz@outlook.com>
2019-01-13 12:33:56 -0300

    Json string

--- YouTView/YouTView/Form1.Designer.cs
+++ YouTView/YouTView/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dgv_VideoList = new System.Windows.Forms.DataGridView();
+            this.wb_playerYTV = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_VideoList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_VideoList
+            // 
+            this.dgv_VideoList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_VideoList.Location = new System.Drawing.Point(12, 234);
+            this.dgv_VideoList.Name = "dgv_VideoList";
+            this.dgv_VideoList.Size = new System.Drawing.Size(568, 150);
+            this.dgv_VideoList.TabIndex = 0;
+            // 
+            // wb_playerYTV
+            // 
+            this.wb_playerYTV.Location = new System.Drawing.Point(12, 12);
+            this.wb_playerYTV.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wb_playerYTV.Name = "wb_playerYTV";
+            this.wb_playerYTV.Size = new System.Drawing.Size(568, 216);
+            this.wb_playerYTV.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(592, 406);
+            this.Controls.Add(this.wb_playerYTV);
+            this.Controls.Add(this.dgv_VideoList);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_VideoList)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgv_VideoList;
+        private System.Windows.Forms.WebBrowser wb_playerYTV;
     }
 }
 
--- YouTView/YouTView/Form1.cs
+++ YouTView/YouTView/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,11 @@ namespace YouTView
         public Form1()
         {
             InitializeComponent();
+        }
+        private void CargarFormulario(object sender, EventArgs e)
+        {
+            var json = new WebClient().DownloadString("https://www.googleapis.com/youtube/v3/search?part=snippet&q=programacion&key=AIzaSyC0EcMARH_nvtnGnO7aCJ4KvB65sJ1mZwA");
+
         }
     }
 }
